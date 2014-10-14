430e7abd52b93ae8004244605d74adcb06e38ea5
daniel5556 <daniel5556@gmail.com>
2014-10-14 18:10:14 -0500

    Revert "Add loyalty points to Player Panel"
    This reverts commit 04998e501404bd8331d9996350db8f7575f30def.

--- src/com/rs/game/player/content/custom/PlayerPanel.java
+++ src/com/rs/game/player/content/custom/PlayerPanel.java
@@ -12,14 +12,22 @@ public class PlayerPanel {
 
 	public static void handleButtons(Player player, int componentId) {
 		if (componentId == 2) {
-			player.getPackets().sendGameMessage("Ability to change password coming soon...",true);
+			player.getPackets().sendGameMessage("Coming soon...",true);
 			return;
 		}
 		else if (componentId == 4) {
-			player.getPackets().sendGameMessage("Titles coming soon!",true);
+			player.getPackets().sendGameMessage("Coming soon...",true);
 			return;
 		}
 		else if (componentId == 6) {
+			player.getPackets().sendGameMessage("Coming soon...",true);
+			return;
+		}
+		else if (componentId == 8) {
+			player.getPackets().sendGameMessage("Coming soon...",true);
+			return;
+		}
+		else if (componentId == 10) {
 			SlayerTask task = player.getSlayerTask();
 			if (player.getSlayerTask() != null) {
 				player.getPackets().sendGameMessage("You're currently assigned to kill "+ task.getName().toLowerCase()+ " only " + task.getAmount()+ " more to go.",true);
@@ -28,16 +36,8 @@ public class PlayerPanel {
 			return;
 			}
 		}
-		else if (componentId == 8) {
-			player.getPackets().sendGameMessage("Coming soon...",true);
-			return;
-		}
-		else if (componentId == 10) {
-			player.getPackets().sendGameMessage("You currently have "+ player.Loyaltypoints +" Loyalty Points.");
-			return;
-		}
 		else if (componentId == 12) {
-			player.getPackets().sendGameMessage("You currently have "+ player.credits +" Credits.");
+			player.getPackets().sendGameMessage("You currently have "+ player.credits +" credits.");
 			return;
 		}
 		else if (componentId == 14) {
